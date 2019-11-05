8032de9a634351f02e91bc3970e2f82cdd8b857c
Dmitrij <dmitrij.vel@live.se>
2019-11-05 23:19:12 +0100

    started work on a cheat sheet for javascript

---
+++ cheat-sheet.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+// delete a key from a key:value object
+const person = {age: 12, name: "John Doe", height: "126cm"}
+
+delete person["height"]; //  person => {age: 12, name: "John Doe"}
