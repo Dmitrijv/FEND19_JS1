a4157b7fea6d517009704a2129c95527388ab81c
Dmitrij <dmitrij.vel@live.se>
2019-11-10 22:13:07 +0100

    added an example for reducing a string in to a hash map

--- cheat-sheet.js
+++ cheat-sheet.js
@@ -50,3 +50,10 @@ const valuesAreEqual = Object.is(val1, val2);
 // reduce object properties
 const salaries = { John: 20000, Ann: 26000, Pete: 24000 };
 const sum = Object.values(salaries).reduce((sum, value) => sum + value, 0); // sum => 70000
+
+
+// count number of times each letter occurs in a string
+const string = 'arithmetics';
+const map = string.split("").reduce((map, letter) => { map[letter] = ~~map[letter] + 1; return map }, {});
+console.log(map); // { a: 1, r: 1, i: 2, t: 2, h: 1, m: 1, e: 1, c: 1, s: 1 }
+
--- test.js
+++ test.js
@@ -1,35 +1,5 @@
 
-const myObj = {
-    a: "23",
-    b: "3",
-};
 
-console.log(delete myObj.a);
-console.log(myObj);
-
-function filterDuplicates(list) {
-
-    console.log(list);
-    const returnList = [];
-
-    for (let i = 0; i < list.length; i++) {
-        if (returnList.includes(list[i]) === false){
-            returnList.push(list[i]);
-        }
-    }
-
-    console.log(returnList);
-    return returnList;
-
-}
-
-const actions = [
-    'LOG_IN',
-    'CALL_SERVER',
-    'UPLOAD',
-    'CALL_SERVER',
-    'CALL_SERVER',
-];
-
-const filteredActions = filterDuplicates(actions);
-
+const string = 'arithmetics';
+const map = string.split("").reduce((map, letter) => { map[letter] = ~~map[letter] + 1; return map}, {});
+console.log(map); // { a: 1, r: 1, i: 2, t: 2, h: 1, m: 1, e: 1, c: 1, s: 1 }
